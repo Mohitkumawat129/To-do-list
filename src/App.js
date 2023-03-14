1d6d044300037d627bbd91b11fcc21a12b8d687b
Mohit kumawat <mohitkumawat129@gmail.com>
2023-03-15 00:04:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Component from "./Component";
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
 
-function App() {
+  const inputChange = (event) => {
+    setInputList(event.target.value);
+  };
+  const itemsList = () => {
+    setItems((preValue) => {
+      return [...preValue, inputList];
+    });
+    setInputList(""); //make our input field empty when 1 item is added so that
+    //user can add more items
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="box">
+          <h1>To do list</h1>
+          <div className="inputField">
+            <input
+              type="text"
+              placeholder="Add an items"
+              onChange={inputChange}
+              value={inputList}
+            />
+            <button onClick={itemsList}>+</button>
+          </div>
+          <ol>
+            {Items.map((val) => {
+              return <Component text={val} />;
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/Component.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Component = (props) => {
+  return <li>{props.text}</li>;
+};
+export default Component;
